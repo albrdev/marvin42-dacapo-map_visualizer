bc58a249be832500689f434ba4e7e1451df249ef
albrdev <albrdev@gmail.com>
2019-11-29 12:20:37 +0100

    common classes

---
+++ Assets/Scripts/Types.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using UnityEngine;
+using Assets.Scripts.Networking;
+
+namespace Assets.Scripts.Types
+{
+    [Serializable]
+    public struct OrientationData
+    {
+        [SerializeField, ReadOnlyProperty]
+        private float m_Velocity;
+        [SerializeField, ReadOnlyProperty]
+        private Quaternion m_Rotation;
+
+        public float Velocity
+        {
+            get { return m_Velocity; }
+            private set { m_Velocity = value; }
+        }
+
+        public Quaternion Rotation
+        {
+            get { return m_Rotation; }
+            private set { m_Rotation = value; }
+        }
+
+        public Vector3 Direction
+        {
+            get { return (Rotation * Vector3.up); }
+        }
+
+        public static unsafe implicit operator OrientationData(CustomPackets.OrientationDataPacket packet)
+        {
+            return new OrientationData(packet);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}({1}, {2})", nameof(OrientationData), Velocity, Rotation);
+        }
+
+        public OrientationData(CustomPackets.OrientationDataPacket value) : this()
+        {
+            Velocity = System.BitConverter.ToSingle(BitConverter.GetBytes(value.Velocity), 0);
+
+            Rotation = new Quaternion
+            (
+                w: System.BitConverter.ToSingle(BitConverter.GetBytes(value.Rotation.W), 0),
+                x: System.BitConverter.ToSingle(BitConverter.GetBytes(value.Rotation.X), 0),
+                y: System.BitConverter.ToSingle(BitConverter.GetBytes(value.Rotation.Y), 0),
+                z: System.BitConverter.ToSingle(BitConverter.GetBytes(value.Rotation.Z), 0)
+            );
+        }
+
+        public OrientationData(float velocity, Quaternion rotation) : this()
+        {
+            Velocity = velocity;
+            Rotation = rotation;
+        }
+    }
+
+    [Serializable]
+    public struct ProximityData
+    {
+        [SerializeField, ReadOnlyProperty]
+        private float m_Distance;
+        [SerializeField, ReadOnlyProperty]
+        private float m_Angle;
+
+        public float Distance
+        {
+            get { return m_Distance; }
+            private set { m_Distance = value; }
+        }
+
+        public float Angle
+        {
+            get { return m_Angle; }
+            private set { m_Angle = value; }
+        }
+
+        public Quaternion Rotation
+        {
+            get { return Quaternion.AngleAxis(Angle, Vector3.back); }
+        }
+
+        public Vector3 Direction
+        {
+            get { return (Rotation * Vector3.up); }
+        }
+
+        public Vector3 Position
+        {
+            get { return Direction * Distance; }
+        }
+
+        public static unsafe implicit operator ProximityData(CustomPackets.ProximityDataPacket packet)
+        {
+            return new ProximityData(packet);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}({1}, {2})", nameof(ProximityData), Distance, Angle);
+        }
+
+        public ProximityData(CustomPackets.ProximityDataPacket value) : this()
+        {
+            Distance = System.BitConverter.ToSingle(BitConverter.GetBytes(value.Distance), 0);
+            Angle = System.BitConverter.ToSingle(BitConverter.GetBytes(value.Angle), 0);
+        }
+
+        public ProximityData(float distance, float angle) : this()
+        {
+            Distance = distance;
+            Angle = angle;
+        }
+    }
+}
