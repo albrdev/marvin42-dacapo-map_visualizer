c05cd75b769f995f98caab86b3d9336b3bc53ed3
albrdev <albrdev@gmail.com>
2019-11-30 23:59:43 +0100

    MonoBehaviour singleton class

---
+++ Assets/Scripts/Components/MonoBehaviourSingleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public abstract class MonoBehaviourSingleton : MonoBehaviour
+{
+    private static readonly Dictionary<System.Type, MonoBehaviourSingleton> s_Instances = new Dictionary<System.Type, MonoBehaviourSingleton>();
+
+    protected static IReadOnlyDictionary<System.Type, MonoBehaviourSingleton> Instances { get { return s_Instances; } }
+
+    public static T GetInstance<T>() where T : MonoBehaviourSingleton
+    {
+        MonoBehaviourSingleton instance;
+        s_Instances.TryGetValue(typeof(T), out instance);
+        return (T)instance;
+    }
+
+    public static MonoBehaviourSingleton Instance
+    {
+        get { return GetInstance<MonoBehaviourSingleton>(); }
+    }
+
+    protected virtual void Awake()
+    {
+        s_Instances.Add(GetType(), this);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        s_Instances.Remove(GetType());
+    }
+}
