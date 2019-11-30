7d6b55b80bcb9f760053dab3212b65b86584e5bb
albrdev <albrdev@gmail.com>
2019-11-30 23:19:48 +0100

    extension classes ++

---
+++ Assets/Scripts/ExtensionClasses/FloatExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Assets.Scripts.ExtensionClasses
+{
+    public static class FloatExtensions
+    {
+        public static float ToPercentage(this float self)
+        {
+            return self * 100f;
+        }
+
+        public static float FromPercentage(this float self)
+        {
+            return self / 100f;
+        }
+    }
+}
