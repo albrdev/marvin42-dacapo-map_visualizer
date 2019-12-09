49107af30c3546e620ed9f9f5e34303941cce252
albrdev <albrdev@gmail.com>
2019-12-09 20:18:42 +0100

    -

--- Assets/Scripts/Networking/Packet.cs
+++ Assets/Scripts/Networking/Packet.cs
@@ -6,7 +6,7 @@ namespace Assets.Scripts.Networking
 {
     public static class Packet
     {
-        public enum Type : byte
+        public enum Type : Byte
         {
             False   = 0,
             True    = 1,
@@ -16,24 +16,24 @@ namespace Assets.Scripts.Networking
 
         public static unsafe bool VerifyHeader(Header* pkt)
         {
-            return CRC16.Generate((byte*)pkt + Marshal.SizeOf(pkt->HeaderChecksum), Marshal.SizeOf(*pkt) - Marshal.SizeOf(pkt->HeaderChecksum)) == pkt->HeaderChecksum;
+            return CRC16.Generate((Byte*)pkt + Marshal.SizeOf(pkt->HeaderChecksum), Marshal.SizeOf(*pkt) - Marshal.SizeOf(pkt->HeaderChecksum)) == pkt->HeaderChecksum;
         }
 
         public static unsafe bool VerifyData(void* pkt)
         {
             Header* hdr = (Header*)pkt;
-            return CRC16.Generate((byte*)pkt + Marshal.SizeOf(*hdr), hdr->Size) == hdr->DataChecksum;
+            return CRC16.Generate((Byte*)pkt + Marshal.SizeOf(*hdr), hdr->Size) == hdr->DataChecksum;
         }
 
-        public static unsafe void GenerateHeader(Header* pkt, UInt16 size, byte type)
+        public static unsafe void GenerateHeader(Header* pkt, UInt16 size, Byte type)
         {
             pkt->Type = type;
             pkt->Size = (UInt16)(size - Marshal.SizeOf(*pkt));
-            pkt->DataChecksum = CRC16.Generate((byte*)pkt + Marshal.SizeOf(*pkt), pkt->Size);
-            pkt->HeaderChecksum = CRC16.Generate((byte*)pkt + Marshal.SizeOf(pkt->HeaderChecksum), Marshal.SizeOf(*pkt) - Marshal.SizeOf(pkt->HeaderChecksum));
+            pkt->DataChecksum = CRC16.Generate((Byte*)pkt + Marshal.SizeOf(*pkt), pkt->Size);
+            pkt->HeaderChecksum = CRC16.Generate((Byte*)pkt + Marshal.SizeOf(pkt->HeaderChecksum), Marshal.SizeOf(*pkt) - Marshal.SizeOf(pkt->HeaderChecksum));
         }
 
-        public static unsafe void GenerateBasic(void* pkt, byte type)
+        public static unsafe void GenerateBasic(void* pkt, Byte type)
         {
             Packet.GenerateHeader((Packet.Header*)pkt, (UInt16)Marshal.SizeOf<Packet.Header>(), type);
         }
@@ -43,10 +43,10 @@ namespace Assets.Scripts.Networking
         {
             public UInt16 HeaderChecksum { get; internal set; }
             public UInt16 DataChecksum { get; internal set; }
-            public byte Type { get; internal set; }
+            public Byte Type { get; internal set; }
             public UInt16 Size { get; internal set; }
 
-            public Header(UInt16 headerChecksum, UInt16 dataChecksum, byte type, UInt16 size) : this()
+            public Header(UInt16 headerChecksum, UInt16 dataChecksum, Byte type, UInt16 size) : this()
             {
                 HeaderChecksum = headerChecksum;
                 DataChecksum = dataChecksum;
